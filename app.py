6bdf697c5633d7f8833d6f5af1a4fa8d25730b35
rushilamla <155717418+rushilamla@users.noreply.github.com>
2025-02-10 14:30:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,9 @@ def predict():
     
     return jsonify({'prediction': result})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get PORT from environment, default to 5000
+    app.run(host='0.0.0.0', port=port)
+
